c140741b28cdff743651b029957947763a64c4c1
miha <miha@WS00800908001.local>
2019-10-22 15:31:39 +0300

    Update db.sqlite3, admin.cpython-37.pyc, and 5 more files...

--- rep_test_00/admin.py
+++ rep_test_00/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Regions
+from .models import Regions, GorRayon
+'''
+class RegionsAdmin(admin.ModelAdmin):
+	list_display = ('name')
+	#list_display_links = ('name')
+'''
+class GorRayonAdmin(admin.ModelAdmin):
+	list_display = ('region', 'name')
+	#list_display_links = ('region', 'name')
+
 
 admin.site.register(Regions)	
+#admin.site.register(Regions, RegionsAdmin)	
+#admin.site.register(GorRayon)
+admin.site.register(GorRayon, GorRayonAdmin)
--- rep_test_00/models.py
+++ rep_test_00/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Regions(models.Model):
 	name = models.CharField(max_length=50)
 
+	def __str__(self):
+		return self.name	
+
+class GorRayon(models.Model):
+	region = models.ForeignKey(Regions, null=True, on_delete=models.PROTECT)
+	name = models.CharField(max_length=50)
