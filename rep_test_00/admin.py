efbd8cdceb94743fd2574b24d052ab34fc86982f
miha <miha@WS00800908001.local>
2019-10-22 15:49:32 +0300

    Создали БД населенных пунктов , пока без заполнения

--- rep_test_00/admin.py
+++ rep_test_00/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Regions, GorRayon
+from .models import Regions, GorRayon, Nsp
 '''
 class RegionsAdmin(admin.ModelAdmin):
 	list_display = ('name')
@@ -12,9 +12,13 @@ class GorRayonAdmin(admin.ModelAdmin):
 	list_display = ('region', 'name')
 	#list_display_links = ('region', 'name')
 
+class NspAdmin(admin.ModelAdmin):
+	list_display = ('region', 'rayon', 'name')
+
 
 admin.site.register(Regions)	
 #admin.site.register(Regions, RegionsAdmin)	
 #admin.site.register(GorRayon)
 admin.site.register(GorRayon, GorRayonAdmin)
+admin.site.register(Nsp, NspAdmin)
 
--- rep_test_00/models.py
+++ rep_test_00/models.py
@@ -3,11 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Regions(models.Model):
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=50, verbose_name='Наименование')
 
 	def __str__(self):
 		return self.name	
 
+	class Meta:
+		verbose_name_plural = 'Регионы'
+		verbose_name = 'Регион'
+
 class GorRayon(models.Model):
-	region = models.ForeignKey(Regions, null=True, on_delete=models.PROTECT)
-	name = models.CharField(max_length=50)
+	region = models.ForeignKey(Regions, null=True, on_delete=models.PROTECT, verbose_name='Регион')
+	name = models.CharField(max_length=50, verbose_name='Наименование')
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = 'Города и(или) Районы'
+		verbose_name = 'Город и(или) Район'
+
+
+class Nsp(models.Model):
+	region = models.ForeignKey(Regions, null=True, on_delete=models.PROTECT, verbose_name='Регион')
+	rayon = models.ForeignKey(GorRayon, null=True, on_delete=models.PROTECT, verbose_name='Район')
+	name = models.CharField(max_length=50, verbose_name='Наименование')
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = 'Населенные пункты'
+		verbose_name = 'Населеный пункт'
