c06f9a9d68ec73bd2a23f083a20cb1677d338b01
miha <miha@WS00800908001.local>
2019-12-26 16:49:46 +0300

    Update add_org.cpython-37.pyc, add_org.py, and 7 more files...

--- rep_test_00/admin.py
+++ rep_test_00/admin.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 
 from .models import Regions, GorRayon, Nsp, Otrasl, PodOtrasl, Org
 
-'''
+
 class RegionsAdmin(admin.ModelAdmin):
-	inlines = (NspInline,)
-	#list_display = ('name')
+	#inlines = (NspInline,)
+	list_display = ('id', 'name')
 	#list_display_links = ('name')
-'''
+
 
 class GorRayonAdmin(admin.ModelAdmin):
 	list_display = ('region', 'name')
@@ -25,17 +25,25 @@ class OrgAdmin(admin.ModelAdmin):
 	list_display = ('inn', 'name', 'otrasl', 'pod_otrasl', 'nsp', 'okved')
 	list_filter = ('region', 'rayon', 'nsp', 'otrasl', 'pod_otrasl',)
 
+class OtraslAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+
+class PodOtraslAdmin(admin.ModelAdmin):
+	list_display = ('id', 'otrasl', 'name')
+	list_filter = ('otrasl',)
+
 #class OtraslAdmin(admin.ModelAdmin):
 #	list_display = ('name')
 
 
-admin.site.register(Regions)	
-#admin.site.register(Regions, RegionsAdmin)	
+#admin.site.register(Regions)	
+admin.site.register(Regions, RegionsAdmin)	
 #admin.site.register(GorRayon)
 admin.site.register(GorRayon, GorRayonAdmin)
 admin.site.register(Nsp, NspAdmin)
 #admin.site.register(Nsp)
-admin.site.register(Otrasl)
-admin.site.register(PodOtrasl)
+admin.site.register(Otrasl, OtraslAdmin)
+admin.site.register(PodOtrasl, PodOtraslAdmin)
 admin.site.register(Org, OrgAdmin)
 
