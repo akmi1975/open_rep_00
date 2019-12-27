7034125fd2b27e1547b98a9a01a0c1c309840c9f
akmi1975 <42997846+akmi1975@users.noreply.github.com>
2019-12-27 23:57:29 +0300

    добавил больше фильтров по отраслям

--- rep_test_00/views.py
+++ rep_test_00/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Org, GorRayon, Nsp, Otrasl, PodOtrasl
+from django.db.models import Q
 
 ME_OTRASL_ID = 0
 ME_PODOTRASL_ID = 0
@@ -59,14 +60,23 @@ def by_otrasl_4(request, otrasl_id, podotrasl_id, rayon_id, nsp_id):
 	Otrasls = Otrasl.objects.filter(org__otrasl__gt=0).distinct()
 	PodOtrasls = PodOtrasl.objects.filter(org__pod_otrasl__gt=0).distinct()
 
-	if ME_PODOTRASL_ID == 0 and ME_NSP_ID == 0:
-		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(rayon=ME_RAYON_ID)
-	elif ME_PODOTRASL_ID != 0 and ME_NSP_ID == 0:
-		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(pod_otrasl=ME_PODOTRASL_ID).filter(rayon=ME_RAYON_ID)
-	elif ME_PODOTRASL_ID == 0 and ME_NSP_ID != 0:
-		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(rayon=ME_RAYON_ID).filter(nsp=ME_NSP_ID)
-	else:
-		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(pod_otrasl=ME_PODOTRASL_ID).filter(rayon=ME_RAYON_ID).filter(nsp=ME_NSP_ID)
+	Orgs = Org.objects.all()
+
+	#links: https://djbook.ru/rel1.8/topics/db/queries.html
+
+	if ME_OTRASL_ID > 0:
+		Orgs = Orgs.filter(otrasl=ME_OTRASL_ID)
+		Rayons = Rayons.filter(org__otrasl=ME_OTRASL_ID)
+		Nsps = Nsps.filter(org__otrasl=ME_OTRASL_ID)
+	if ME_PODOTRASL_ID > 0:
+		Orgs = Orgs.filter(pod_otrasl=ME_PODOTRASL_ID)
+		Rayons = Rayons.filter(org__pod_otrasl=ME_PODOTRASL_ID)
+		Nsps = Nsps.filter(org__pod_otrasl=ME_PODOTRASL_ID)
+	if ME_RAYON_ID > 0:
+		Orgs = Orgs.filter(rayon=ME_RAYON_ID)
+	if ME_NSP_ID > 0:
+		Orgs = Orgs.filter(nsp=ME_NSP_ID)
+
 	#Orgs = Org.objects.all()
 	context = {'Rayons':Rayons, 'Nsps':Nsps, 'Otrasls':Otrasls, 'PodOtrasls':PodOtrasls, 'me_otr':ME_OTRASL_ID, 'me_podotr':ME_PODOTRASL_ID, 'me_ray': ME_RAYON_ID, 'me_nsp':ME_NSP_ID, 'Orgs':Orgs }
 
