dbf88abf8c4d3e1360200456c2ff6c0f1d307e8e
akmi1975 <42997846+akmi1975@users.noreply.github.com>
2019-12-27 20:16:19 +0300

    выборка по населенным пунктам и отраслям

--- rep_test_00/urls.py
+++ rep_test_00/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, by_otrasl, by_otrasl_2
+from .views import index, by_otrasl, by_otrasl_2, by_otrasl_4
 
 
 urlpatterns = [
     path('', index),
     path('<int:otrasl_id>/', by_otrasl, name='by_otrasl'),
     path('<int:otrasl_id>/<int:podotrasl_id>/', by_otrasl_2, name='by_otrasl_2'),
+    path('<int:otrasl_id>/<int:podotrasl_id>/<int:rayon_id>/<int:nsp_id>/', by_otrasl_4, name='by_otrasl_4'),
 ]
--- rep_test_00/views.py
+++ rep_test_00/views.py
@@ -5,6 +5,8 @@ from .models import Org, GorRayon, Nsp, Otrasl, PodOtrasl
 
 ME_OTRASL_ID = 0
 ME_PODOTRASL_ID = 0
+ME_RAYON_ID = 0
+ME_NSP_ID = 0
 
 def index(request):
 	#template = loader.get_template('org_selector/index.html')
@@ -13,10 +15,12 @@ def index(request):
 	Otrasls = Otrasl.objects.filter(org__otrasl__gt=0).distinct()
 	PodOtrasls = PodOtrasl.objects.filter(org__pod_otrasl__gt=0).distinct()
 	#Orgs = Org.objects.all()
-	context = {'Rayons':Rayons, 'Nsps':Nsps, 'Otrasls':Otrasls, 'PodOtrasls':PodOtrasls }
+	#context = {'Rayons':Rayons, 'Nsps':Nsps, 'Otrasls':Otrasls, 'PodOtrasls':PodOtrasls }
+	context = {'Rayons':Rayons, 'Nsps':Nsps, 'Otrasls':Otrasls, 'PodOtrasls':PodOtrasls, 'me_otr':ME_OTRASL_ID, 'me_podotr':ME_PODOTRASL_ID, 'me_ray': ME_RAYON_ID, 'me_nsp':ME_NSP_ID }
+
 	#context = {'Orgs':Orgs}
 	#return HttpResponse(template.render(context, request))
-	return render(request, 'org_selector/index.html', context)
+	return render(request, 'org_selector/index_1.html', context)
 
 def by_otrasl(request, otrasl_id):
 	ME_OTRASL_ID = otrasl_id
@@ -44,4 +48,27 @@ def by_otrasl_2(request, otrasl_id, podotrasl_id):
 
 	return render(request, 'org_selector/index.html', context)
 
+def by_otrasl_4(request, otrasl_id, podotrasl_id, rayon_id, nsp_id):
+	ME_OTRASL_ID = otrasl_id
+	ME_PODOTRASL_ID = podotrasl_id
+	ME_RAYON_ID = rayon_id
+	ME_NSP_ID = nsp_id
+
+	Rayons = GorRayon.objects.filter(org__rayon__gt=0).distinct()
+	Nsps = Nsp.objects.filter(org__nsp__gt=0).distinct()
+	Otrasls = Otrasl.objects.filter(org__otrasl__gt=0).distinct()
+	PodOtrasls = PodOtrasl.objects.filter(org__pod_otrasl__gt=0).distinct()
+
+	if ME_PODOTRASL_ID == 0 and ME_NSP_ID == 0:
+		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(rayon=ME_RAYON_ID)
+	elif ME_PODOTRASL_ID != 0 and ME_NSP_ID == 0:
+		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(pod_otrasl=ME_PODOTRASL_ID).filter(rayon=ME_RAYON_ID)
+	elif ME_PODOTRASL_ID == 0 and ME_NSP_ID != 0:
+		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(rayon=ME_RAYON_ID).filter(nsp=ME_NSP_ID)
+	else:
+		Orgs = Org.objects.filter(otrasl=ME_OTRASL_ID).filter(pod_otrasl=ME_PODOTRASL_ID).filter(rayon=ME_RAYON_ID).filter(nsp=ME_NSP_ID)
+	#Orgs = Org.objects.all()
+	context = {'Rayons':Rayons, 'Nsps':Nsps, 'Otrasls':Otrasls, 'PodOtrasls':PodOtrasls, 'me_otr':ME_OTRASL_ID, 'me_podotr':ME_PODOTRASL_ID, 'me_ray': ME_RAYON_ID, 'me_nsp':ME_NSP_ID, 'Orgs':Orgs }
+
+	return render(request, 'org_selector/index_1.html', context)
 
