89acf55cd004ab663afc8245211327a0f06d83a6
miha <pd4@yandex.ru>
2020-01-29 16:30:29 +0300

    Рисуем интерфейс bootstrap

--- rep_test_00/my_func.py
+++ rep_test_00/my_func.py
@@ -1,4 +1,4 @@
-from .models import Org, GorRayon, Nsp, Otrasl, PodOtrasl, orgContact
+from .models import Org, GorRayon, Nsp, Otrasl, PodOtrasl, orgContact, Sheff
 #from django.db.models import Q
 from django.core.paginator import Paginator
 
@@ -34,6 +34,7 @@ def basic_filtr_or(request, vid_sel, vid_1_id, pod_vid_1_id, vid_2_id, pod_vid_2
 		if ME_PODVID2_ID != 0:
 			orgs = orgs.filter(pod_otrasl=ME_PODVID2_ID)
 			OrgContact = OrgContact.filter(org__pod_otrasl__exact=ME_PODVID2_ID)
+		name_vid = ['Район', 'Отрасль']
 		
 	elif vid_sel == 1:
 		vid_1 = Otrasl.objects.filter(org__otrasl__gt=0).distinct()
@@ -56,13 +57,17 @@ def basic_filtr_or(request, vid_sel, vid_1_id, pod_vid_1_id, vid_2_id, pod_vid_2
 		if ME_PODVID2_ID != 0:
 			orgs = orgs.filter(nsp=ME_PODVID2_ID)
 			OrgContact = OrgContact.filter(org__nsp__exact=ME_PODVID2_ID)
+		name_vid = ['Отрасль','Район']
 
-	paginator = Paginator(orgs, 2)
+
+	paginator = Paginator(orgs, 1)
 	if 'page' in request.GET:
 		page_num = request.GET['page']
 	else:
 		page_num = 1
 	page = paginator.get_page(page_num)
 
-	return {'href_vid_sel':'/' + str(vs) + '/0/0/0/0/', 'vid_sel':vid_sel, 'vid_1':vid_1, 'vid_2':vid_2, 'pod_vid_1':pod_vid_1,'pod_vid_2':pod_vid_2, 'me_vid1':ME_VID1_ID, 'me_podvid1':ME_PODVID1_ID, 'me_vid2':ME_VID2_ID, 'Orgs':page.object_list, 'OrgContact':OrgContact, 'page':page}
+	sheff = Sheff.objects.get(org=page.object_list[0])
+
+	return {'href_vid_sel':'/' + str(vs) + '/0/0/0/0/', 'vid_sel':vid_sel, 'vid_1':vid_1, 'vid_2':vid_2, 'pod_vid_1':pod_vid_1,'pod_vid_2':pod_vid_2, 'me_vid1':ME_VID1_ID, 'me_podvid1':ME_PODVID1_ID, 'me_vid2':ME_VID2_ID, 'me_podvid2':ME_PODVID2_ID, 'Orgs':page.object_list, 'OrgContact':OrgContact, 'page':page, 'name_vid':name_vid, 'sheff':sheff}
 
